55de07c1bd3fd70eb7d1f0766f0c6240fc096560
fpd.core20@gmail.com <fpd.core20@gmail.com>
2025-08-19 02:05:26 +0300

    develop branch initialize

---
+++ BLL/Abstractions/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using BLL.Dto;
+
+namespace BLL.Abstractions
+{
+    public interface IUserService
+    {
+        public Task<CallbackDto<bool>> DeleteUser(DeleteDto dto);
+        public Task<CallbackDto<bool>> UpdateUser(UpdateUserDto dto);
+        public Task<CallbackDto<FullUserDto>> GetUserByLogin(string login);
+        public Task<CallbackDto<FullUserDto>> GetUser(Guid id);
+        public Task<CallbackDto<string>> AuthUser(UserDto dto);
+        public Task<CallbackDto<bool>> AddUser(UserDto dto);
+    }
+}
